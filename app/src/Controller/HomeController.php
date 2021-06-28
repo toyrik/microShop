831a851301f628eed571ad00bc00a29d05414330
toyrik <toyrik@gmail.com>
2021-06-28 22:40:30 +0300

    Geting form data

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -6,17 +6,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home" , methods={"get"})
      * @return Response
      */
     public function index(): Response
     {
         $number = random_int(0,100);
+        return $this->render('home/index.html.twig',[
+            'number' => $number,
+        ]);
+    }
+
+    /**
+     * @Route("/", name="posHome" , methods={"post"})
+     * @return Response
+     */
+    public function list(Request $request): Response
+    {
+        $number = ($request->request->get('number')) ? $request->request->get('number') : random_int(0,100);
+
         return $this->render('home/index.html.twig',[
             'number' => $number,
         ]);
