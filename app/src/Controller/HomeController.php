ee81841d70e63804d8d43d22c99761aebd4349d5
toyrik <toyrik@gmail.com>
2021-06-29 15:26:11 +0300

    Добавляем форму на главную страницу

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,11 +3,15 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Item;
+use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class HomeController extends AbstractController
 {
@@ -17,9 +21,12 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $form = $this->getFilterForm();
+
         $number = random_int(0,100);
         return $this->render('home/index.html.twig',[
-            'number' => $number,
+            'form' => $form->createView(),
+
         ]);
     }
 
@@ -29,10 +36,50 @@ class HomeController extends AbstractController
      */
     public function list(Request $request): Response
     {
+        $form = $this->getFilterForm();
         $number = ($request->request->get('number')) ? $request->request->get('number') : random_int(0,100);
 
         return $this->render('home/index.html.twig',[
-            'number' => $number,
+            'form' => $form->createView(),
         ]);
+    }
+
+    protected function getFilterForm()
+    {
+        return $this->createFormBuilder()
+            ->add('with', ChoiceType::class, array(
+                'multiple' => true,
+                'attr' => ['class' => 'form-select filter'],
+                'label' => 'Включить',
+                'required' => false,
+                'label_attr' => ['class' => 'label'],
+                'choices' => array(
+                    'Одежда' => 1,
+                    'Обувь' => 2,
+                    'Стиль' => 3,
+                    'Повседневное' => 4,
+                    'Черное' => 5,
+                    'Белое' => 6,
+                ),
+            ))
+            ->add('without', ChoiceType::class, array(
+                'multiple' => true,
+                'attr' => ['class' => 'form-select filter'],
+                'label' => 'Исключить',
+                'required' => false,
+                'label_attr' => ['class' => 'label'],
+                'choices' => array(
+                    'Одежда' => 1,
+                    'Обувь' => 2,
+                    'Стиль' => 3,
+                    'Повседневное' => 4,
+                    'Черное' => 5,
+                    'Белое' => 6,
+                ),
+            ))
+            ->add('Filter', SubmitType::class, array(
+                'attr' => ['class' => 'btn btn-primary']
+            ))
+            ->getForm();
     }
 }
